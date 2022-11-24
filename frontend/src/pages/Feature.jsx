123b2dcfd53c5322d1b03a13a1f5f135752f3654
Orenikoe <orental21@gmail.com>
2022-11-24 19:17:32 +0000

    completed

--- frontend/src/pages/Feature.jsx
+++ frontend/src/pages/Feature.jsx
@@ -16,7 +16,7 @@ export default function Feature() {
   const [chooseBackground, setChooeseBackground] = useState(null);
   const [isCompleted, setIsCompleted] = useState(null);
   return (
-    <div>
+    <div className="page">
       <Snowfall />
       <div className="app-left-side">
         <MoreCardButton whenClicked={setIsLibraryOpen} />
@@ -33,7 +33,9 @@ export default function Feature() {
           reset={setIsCompleted}
         />
       </div>
-      {textValue === null && <div>Fill the Message First!!</div>}
+      {textValue === null && (
+        <h3 className="instructions">Fill the Message First!!</h3>
+      )}
       {isCompleted && textValue !== null && (
         <div className="final-result">
           <h2>Your Card is ready</h2>
